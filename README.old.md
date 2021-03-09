5b006718ca54b3b8f012bff1b08c9762c77d6d93
z2zone <hjun1114@gmail.com>
2021-03-08 21:27:39 -0800

    useState and useEffect testing

---
+++ README.old.md
@@ -0,0 +1,1 @@
+# practicing new hook system
---
+++ src/components/SearchBar.js
@@ -0,0 +1,11 @@
+import React, {setState, useEffect} from 'react';
+
+const SearchBar = () => {
+    return (
+        <div>
+            <input/>
+        </div>
+    )
+}
+
+export default SearchBar;
