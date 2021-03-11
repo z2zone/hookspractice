b26b0a39f6150d63e9e652238f3d72cb610661f6
z2zone <hjun1114@gmail.com>
2021-03-10 23:04:07 -0800

    adding wikipedia search api

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,15 +1,41 @@
-import React, {setState, useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 const SearchBar = () => {
+    
+    const [searchTerm, setTerm] = useState(''); 
+
+    function onInputChange(e){
+        setTerm(e.target.value);
+    }
+
+    useEffect(()=>{
+        const search = async ()=>{
+            await axios.get('https://en.wikipedia.org/w/api.php',{
+                params: {
+                    action: 'query',
+                    list : 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: searchTerm
+                }
+            }); 
+        };
+        search();
+    }, [searchTerm]);
+
     return (
         <form className="ui form">
             <div className="field">
                 <label>Search Bar</label>
-                <input/>
+                <input 
+                    value ={searchTerm}
+                    onChange = {onInputChange}
+                    className="input"
+                />
             </div>
         </form>
-
     )
-}
+};
 
 export default SearchBar;
